3a2cca4e3be052b6516380f3298104d1dff6ea40
Krzysztof Pajak <kpajak@gmail.com>
2014-07-03 02:20:51 +0200

    messages and users list handling

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,11 +1,56 @@
-var socket = null;
+var socket = null,
+    author = null;
 
 $(document).ready(function () {
     socket = io();
-    socket.on('connect', onConnect);
+    socket.on("connect", onConnect);
+    socket.on("authenticate", onAuthenticate);
+
+    $("#signinForm").submit(function (event) {
+        socket.emit('authenticate', $('#username').val(), $('#email').val());
+        event.preventDefault();
+    });
 });
 
 function onConnect () {
     console.log('connected');
-    socket.emit('authenticate', 'exogeneick', 'kpajak@gmail.com');
+}
+
+function onAuthenticate (response) {
+    if (1 === response.code || 2 === response.code) {
+        author = $('#username').val();
+
+        $('#signinForm').fadeOut('slow', function () {
+            $('#chatRoom').fadeIn('slow', function () {
+                socket.on("message", onMessage);
+                socket.on("users", onUsers);
+
+                socket.emit("users");
+
+                $("#sendMessageButton").on("click", function () {
+                    socket.emit("message", author, $("#message").val());
+                });
+            });
+        });
+        return;
+    }
+
+    $('#signinError').fadeIn('slow', function () {});
+}
+
+function onMessage (message) {
+    $("#messages").append('<div class="media"><strong class="pull-left">' + message.author + '</strong><div class="media-body">' + message.message + '</div></div>');
+    $('#messages').stop().animate({
+        scrollTop: $("#messages")[0].scrollHeight
+    }, 800);
+}
+
+function onUsers (users) {
+    console.log(users);
+    $("#users").html('');
+    $("#users").append('<a href="#" class="list-group-item active">Users online</a>');
+
+    users.forEach(function (user) {
+        $("#users").append('<a href="#" class="list-group-item">' + user + '</a>');
+    });
 }
--- server/chat-server.js
+++ server/chat-server.js
@@ -3,22 +3,57 @@ var ChatServer = function (io) {
 
     self.io = io;
 
+    self.users = [];
+
     self.init = function () {
         self.io.on('connection', self.onClientConnect);
     };
 
     self.onClientConnect = function (socket) {
-        console.log('connected');
         self.handleClient(socket);
     };
 
     self.handleClient = function (socket) {
-        console.log('handling');
         socket.on('authenticate', self.handleAuthenticate);
+        socket.on('message', self.handleMessage);
+        socket.on('users', self.handleUsers);
     };
 
     self.handleAuthenticate = function (username, email) {
-        console.log(username + " - " + email);
+        var i = self.users.length;
+        while (i--) {
+            if (username === self.users[i].username && email === self.users[i].email) {
+                self.io.emit('authenticate', {code: 1, response: {message: "Success"}});
+                return;
+            }
+
+            if (username === self.users[i].username && email != self.users[i].email) {
+                self.io.emit('authenticate', {code: 0, response: {message: "Wrong email"}});
+                return;
+            }
+
+            if (username != self.users[i].username && email === self.users[i].email) {
+                self.io.emit('authenticate', {code: 0, response: {message: "Wrong username"}});
+                return;
+            }
+        }
+
+        self.users.push({username:username, email:email});
+        self.io.emit('authenticate', {code: 2, response: {message: "Account created"}});
+    };
+
+    self.handleMessage = function (author, message) {
+        self.io.emit('message', {author: author, message: message});
+    };
+
+    self.handleUsers = function () {
+        var i = self.users.length,
+            users = [];
+        while (i--) {
+            users.push(self.users[i].username);
+        }
+
+        self.io.emit('users', users);
     };
 };
 
